9b860c6a8da611405ab2ed1197f82f270105a4b4
tsangsimon <simon.tsang@gmail.com>
2015-11-04 09:49:58 +0000

    reverting changes to jenkins.sh as the jenkins job is failing to run the script rather than executing an action within the script

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,31 +1,25 @@
 #! /usr/bin/env bash
-echo "setting env_dir"
+
 env_dir="$JENKINS_HOME/virtualenv/${JOB_NAME// /_}"
 
-echo "create and activate a virtualenv"
 #create and activate a virtualenv
 virtualenv $env_dir
 . $env_dir/bin/activate
 
-echo "install requirements"
 #install requirements
 pip install -r requirements.txt
 
-echo "install test only requirements"
 #install test only requirements
 pip install -r requirements_test.txt
 
-echo "run tests.sh"
 ./test.sh
 
 test_pass=$?
 
-echo "run liniting"
 ./run_linting.sh
 
 python_linting=$?
 
-echo "set coverage"
 coverage xml
 coverage -rm
 
