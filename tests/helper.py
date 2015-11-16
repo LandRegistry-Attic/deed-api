659ab807afa78db2a411dc07869b4081c647ce2c
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2015-11-16 12:01:18 +0000

    Unit Tests updated to include borrower in original tests

--- tests/helper.py
+++ tests/helper.py
@@ -4,6 +4,32 @@ class DeedModelMock:
 
 
 class DeedHelper:
-    _json_doc = {"title_number": "DN100"}
+    _json_doc = {
+                "title_number": "DN100",
+                "borrowers": [
+                    {
+                        "forename": "fred",
+                        "middle_name": "joe",
+                        "surname": "bloggs"
+                    },
+                    {
+                        "forename": "lisa",
+                        "surname": "bloggette"
+                    }
+                ]
+            }
 
-    _invalid_title = {"title_number": "BBBB12313212BB"}
+    _invalid_title = {
+                    "title_number": "BBBB12313212BB",
+                    "borrowers": [
+                        {
+                            "forename": "fred",
+                            "middle_name": "joe",
+                            "surname": "bloggs"
+                        },
+                        {
+                            "forename": "lisa",
+                            "surname": "bloggette"
+                        }
+                    ]
+                }
