66015d61f281972deb8a9ab14f01e5a8043d0222
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2015-11-16 12:08:30 +0000

    Flake8 fixes completed

--- tests/helper.py
+++ tests/helper.py
@@ -5,31 +5,31 @@ class DeedModelMock:
 
 class DeedHelper:
     _json_doc = {
-                "title_number": "DN100",
-                "borrowers": [
-                    {
-                        "forename": "fred",
-                        "middle_name": "joe",
-                        "surname": "bloggs"
-                    },
-                    {
-                        "forename": "lisa",
-                        "surname": "bloggette"
-                    }
-                ]
+        "title_number": "DN100",
+        "borrowers": [
+            {
+                "forename": "fred",
+                "middle_name": "joe",
+                "surname": "bloggs"
+            },
+            {
+                "forename": "lisa",
+                "surname": "bloggette"
             }
+        ]
+    }
 
     _invalid_title = {
-                    "title_number": "BBBB12313212BB",
-                    "borrowers": [
-                        {
-                            "forename": "fred",
-                            "middle_name": "joe",
-                            "surname": "bloggs"
-                        },
-                        {
-                            "forename": "lisa",
-                            "surname": "bloggette"
-                        }
-                    ]
-                }
+        "title_number": "BBBB12313212BB",
+        "borrowers": [
+            {
+                "forename": "fred",
+                "middle_name": "joe",
+                "surname": "bloggs"
+            },
+            {
+                "forename": "lisa",
+                "surname": "bloggette"
+            }
+        ]
+    }
