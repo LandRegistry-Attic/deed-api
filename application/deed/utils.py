7558191951cb360fddc12de991bf0d4de7aae293
marknorman <mark@kirosoft.co.uk>
2015-11-20 13:44:15 +0000

    refactor to remove extra mutable count variable

--- application/deed/utils.py
+++ application/deed/utils.py
@@ -5,12 +5,13 @@ import json
 
 def validate_helper(json_to_validate):
     error_message = ""
-    error_list_iterable = _title_validator.iter_errors(json_to_validate)
+    error_list = sorted(_title_validator.iter_errors(json_to_validate),
+                        key=str, reverse=True)
 
-    for count, error in enumerate(sorted(error_list_iterable, key=str, reverse=True), start=1):
+    for count, error in enumerate(error_list, start=1):
         error_message += "Problem %s:\n\n%s\n\n" % (count, str(error))
 
-    return count, error_message
+    return len(error_list), error_message
 
 
 def _load_json_schema():
