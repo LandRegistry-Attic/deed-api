6d32fcdce44516d7a48956a4c1d99c9af29e1691
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2015-11-23 09:47:41 +0000

    updated unit tests to validate updated schema

--- tests/helper.py
+++ tests/helper.py
@@ -7,15 +7,24 @@ class DeedHelper:
     _json_doc = {
         "title_number": "DN100",
         "borrowers": [
-            {
-                "forename": "fred",
-                "middle_name": "joe",
-                "surname": "bloggs"
-            },
-            {
-                "forename": "lisa",
-                "surname": "bloggette"
-            }
+        {
+            "forename": "lisa",
+            "middle_name": "ann",
+            "surname": "bloggette",
+            "gender": "M",
+            "address": "test address with postcode, PL14 3JR",
+            "dob": "23/01/1986",
+            "phone_number": "07502154062"
+        },
+        {
+            "forename": "frank",
+            "middle_name": "ann",
+            "surname": "bloggette",
+            "gender": "M",
+            "address": "Test Address With Postcode, PL14 3JR",
+            "dob": "23/01/1986",
+            "phone_number": "07502154062"
+        }
         ]
     }
 
