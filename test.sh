21563678332ba129a5668152d7f686e5cee8b6a4
endamccormack <endamccormack0@gmail.com>
2015-11-27 08:31:06 +0000

    refactored out tests to unit tests

--- test.sh
+++
@@ -1,5 +0,0 @@
-export SETTINGS="config.DevelopmentConfig"
-
-mkdir -p test-reports
-
-py.test --junitxml=test-reports/TEST-dm-deed-api.xml --cov-report term-missing --cov-report=html --cov application tests
