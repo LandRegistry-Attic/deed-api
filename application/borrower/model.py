b982663d3b02503b43274bd5725638fba61d8615
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2016-01-07 11:46:32 +0000

    added token column to database and model

--- application/borrower/model.py
+++ application/borrower/model.py
@@ -1,10 +1,12 @@
 from application import db
+import uuid
 
 
 class Borrower(db.Model):
     __tablename__ = 'borrower'
 
     id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String, nullable=False)
     forename = db.Column(db.String, nullable=False)
     middlename = db.Column(db.String, nullable=True)
     surname = db.Column(db.String, nullable=False)
@@ -12,6 +14,10 @@ class Borrower(db.Model):
     gender = db.Column(db.String, nullable=True)
     phonenumber = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
+
+    @staticmethod
+    def generate_token():
+        return str(uuid.uuid4().hex[:6]).lower()
 
     def save(self):
         db.session.add(self)
