1ff8184fadcfc168fd4fc3b1ee1cff70e7c1d6e1
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2016-02-03 10:36:27 +0000

    Unit tests corrected

--- unit_tests/test_app.py
+++ unit_tests/test_app.py
@@ -19,11 +19,12 @@ class TestRoutes(unittest.TestCase):
         self.app = app.test_client()
 
     def test_health(self):
-        self.assertEqual((self.app.get('/health')).status, '200 OK')
+        self.assertEqual((self.app.get('/health')).status_code,
+                         status.HTTP_200_OK)
 
     def test_deed(self):
-        self.assertEqual((self.app.get('/deed')).status,
-                         '301 MOVED PERMANENTLY')
+        self.assertEqual((self.app.get('/deed')).status_code,
+                         status.HTTP_400_BAD_REQUEST)
 
     def test_model(self):
         test_deed = Deed()
