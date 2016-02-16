187595c618022aa0290630780ecb0e1e4f9cf539
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2016-02-16 11:40:54 +0000

    Config added for Akuma

--- application/config.py
+++ application/config.py
@@ -3,3 +3,6 @@ import os
 DEBUG = os.getenv("DEBUG", True)
 SQLALCHEMY_DATABASE_URI = os.getenv('DEED_DATABASE_URI',
                                     'postgres:///deed_api')
+
+AKUMA_URI = os.getenv('AKUMA_ADDRESS',
+                      'http://10.10.10.10:5055')
