329a1569442b3139035eaa7e220e98526fdadf96
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2016-02-22 10:08:41 +0000

    added trailing / to interface and renamed variable in config.py

--- application/config.py
+++ application/config.py
@@ -4,5 +4,5 @@ DEBUG = os.getenv("DEBUG", True)
 SQLALCHEMY_DATABASE_URI = os.getenv('DEED_DATABASE_URI',
                                     'postgres:///deed_api')
 
-AKUMA_URI = os.getenv('AKUMA_ADDRESS',
+AKUMA_BASE_HOST = os.getenv('AKUMA_ADDRESS',
                       'http://10.10.10.10:5055')
--- application/service_implimentation/akuma/interface.py
+++ application/service_implimentation/akuma/interface.py
@@ -6,7 +6,7 @@ from flask.ext.api import status
 def perform_check(payload):  # pragma: no cover
     data = None
 
-    resp = requests.get(config.AKUMA_BASE_HOST,json=payload)
+    resp = requests.get(config.AKUMA_BASE_HOST + "/", json=payload)
 
     if resp.status_code == status.HTTP_200_OK:
         data = resp.json()
