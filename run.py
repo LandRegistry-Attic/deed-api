318497a937917b7adbec53b36eed685d8f93ab8f
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2016-02-26 10:49:02 +0000

    removed creating of the logs directory to run.py

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from application import app
 import os
 
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
 app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", "5005")))
--- run.sh
+++ run.sh
@@ -2,8 +2,4 @@ export SETTINGS="config.DevelopmentConfig"
 
 python manage.py db upgrade
 
-mkdir logs
-
-chmod 777 logs
-
 python run.py
