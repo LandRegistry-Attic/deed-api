442494a50720fe4cc5628ad779791c5f2125ceb5
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2016-02-26 13:21:09 +0000

    moved log folder creation to jenkins.sh so it is only run on jenkins

--- logger/logging_config.py
+++ logger/logging_config.py
@@ -7,9 +7,6 @@ import os
 @call_once_only
 def setup_logging():
     try:
-        if not os.path.exists("../logs"):
-            os.makedirs("../logs")
-
         with open('logger/logging_config.json', 'rt') as file:
             config = json.load(file)
             dictConfig(config)
