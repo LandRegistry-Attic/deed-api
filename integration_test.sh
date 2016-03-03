9f301d0616f03baae0b4073807248d25f33e586d
Abbas Ghulam <g.abbas@kainos.com>
2016-03-03 17:25:24 +0000

    added dollar sign in front of environment variables

--- integration_test.sh
+++ integration_test.sh
@@ -5,10 +5,10 @@ py.test --junitxml=TEST-INT-flask-app-medium.xml --cov-report term-missing --cov
 # This file will prepare all dependancies for the application
 DB_NAME = "${DEED_DATABASE_NAME:-deed_api}"
 
-psql DB_NAME -c "drop table alembic_version cascade;"
-psql DB_NAME -c "drop table deed cascade;"
-psql DB_NAME -c "drop table borrower cascade;"
-psql DB_NAME -c "drop table mortgage_document cascade;"
+psql $DB_NAME -c "drop table alembic_version cascade;"
+psql $DB_NAME -c "drop table deed cascade;"
+psql $DB_NAME -c "drop table borrower cascade;"
+psql $DB_NAME -c "drop table mortgage_document cascade;"
 
 python3 manage.py db upgrade
 
