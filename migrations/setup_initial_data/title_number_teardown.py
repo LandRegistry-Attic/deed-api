09c617e1a18b472f83f82f52d2b30dd462e9ef33
tsangsimon <simon.tsang@gmail.com>
2016-03-21 14:34:45 +0000

    tweak to make it remove any name:value pairs

--- migrations/setup_initial_data/title_number_teardown.py
+++ migrations/setup_initial_data/title_number_teardown.py
@@ -10,8 +10,12 @@ DB = "postgresql://vagrant:vagrant@localhost:5432/deed_api"
 def process_input_options():
     parser = argparse.ArgumentParser(prog='title_number_teardown', description='utility to remove '
                                      'all acceptance test title numbers.')
+    parser.add_argument('element', metavar='element',
+                        help='prefix of the test title numbers to be removed')
     parser.add_argument('prefix', metavar='prefix',
                         help='prefix of the test title numbers to be removed')
+    parser.add_argument('confirm', metavar='confirm',
+                        help="confirms you want to execute the delete")
 
     settings = parser.parse_args()
     return settings
@@ -22,19 +26,20 @@ def run_teardown():
     settings = process_input_options()
     LOGGER.info("Settings from command line: %s" % str(settings))
 
-    engine = create_engine(DB, convert_unicode=True)
-    sql_connection = engine.connect()
+    if settings.confirm is not None:
+        engine = create_engine(DB, convert_unicode=True)
+        sql_connection = engine.connect()
 
-    sql_text = ("DELETE FROM deed WHERE deed ->> 'title_number' LIKE %s;")
+        sql_text = ("DELETE FROM deed WHERE deed ->> %s LIKE %s;")
 
-    try:
-        params = (settings.prefix + "%")
-        res = sql_connection.execute(sql_text, params)
-        print ("Deleted %s rows" % res.rowcount)
-        return 1
-    except:
-        print("fail")
-        return 0
+        try:
+            params = (settings.element, settings.prefix + "%")
+            res = sql_connection.execute(sql_text, params)
+            print("Deleted %s rows" % res.rowcount)
+            return 1
+        except:
+            print("unable to execute SQL")
+            return 0
 
 
 if __name__ == "__main__":
