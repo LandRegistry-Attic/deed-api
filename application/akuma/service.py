905825594de7fb5e556ecb1b50666e05e6102a8c
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2016-04-05 08:40:33 +0100

    Org locale is sent to Akuma - but not stored

--- application/akuma/service.py
+++ application/akuma/service.py
@@ -4,15 +4,16 @@ from application.service_clients import akuma
 class Akuma:
 
     @staticmethod
-    def do_check(json_payload, check_type, org_name):
+    def do_check(json_payload, check_type, org_name, org_locale):
 
         json_payload['title_no'] = str(json_payload['title_number'])
 
         payload = {
             "service": "digital mortgage",
             "activity": check_type,
-            "organisation ID": org_name,
-            "payload": json_payload
+            "organisation_name": org_name,
+            "payload": json_payload,
+            "organisation_locale" : org_locale
         }
 
         akuma_client = akuma.make_akuma_client()
