8a905a10da74fb7d38fecf19bc49e4b2283bc886
phemus <ramin.vazir@landregistry.gsi.gov.uk>
2016-06-20 11:31:03 +0100

    changed integration test data

--- integration_tests/deed/deed_data.py
+++ integration_tests/deed/deed_data.py
@@ -1,26 +1,16 @@
 valid_deed = {
-    "title_number": "DN100",
+    "title_number": "GR515835",
     "md_ref": "e-MD123G",
     "property_address": "5 The Drive, This Town, This County, PL4 4TH",
     "identity_checked": "Y",
     "borrowers": [
         {
-            "forename": "lisa",
-            "middle_name": "ann",
-            "surname": "bloggette",
+            "forename": "Ann",
+            "surname": "Smith",
             "gender": "Male",
             "address": "test address with postcode, PL14 3JR",
             "dob": "23/01/1986",
             "phone_number": "07502154062"
-        },
-        {
-            "forename": "frank",
-            "middle_name": "ann",
-            "surname": "bloggette",
-            "gender": "Female",
-            "address": "Test Address With Postcode, PL14 3JR",
-            "dob": "23/01/1986",
-            "phone_number": "07502154061"
         }
     ]
 }
