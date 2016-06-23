764872a5006ae3cedda5f5e935d7a709054094f7
treacle74 <ptrelease@gmail.com>
2016-06-23 14:47:24 +0100

    decoupled logging dependencies, so that modules that import app can be run independently, without file not found errors (owing to absolute file path dependencies of the logger).

--- logger/logging_config.py
+++ logger/logging_config.py
@@ -1,4 +1,5 @@
 import json
+import os
 from logging.config import dictConfig  # type: ignore
 from logger.utils import call_once_only
 
@@ -6,8 +7,14 @@ from logger.utils import call_once_only
 @call_once_only
 def setup_logging():
     try:
-        with open('logger/logging_config.json', 'rt') as file:
+        dirname = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(dirname, 'logging_config.json'), 'rt') as file:
             config = json.load(file)
+            application_path = os.path.split(os.path.abspath('application'))[0]
+            project_path = os.path.split(application_path)[0]
+            config['handlers']['log_file']['filename'] = \
+                os.path.join(project_path, 'logs/dm-api-info.log')
+
             dictConfig(config)
     except IOError as e:
         raise(Exception('Failed to load logging configuration', e))
