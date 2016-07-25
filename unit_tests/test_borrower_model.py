a620985c963fca2c7a068f866d7978c4c41bb770
Ramin <ramin.vazir@digital.landregistry.gov.uk>
2016-07-25 14:34:31 +0100

    unhappy path test for get_borrower_by_id

--- unit_tests/test_borrower_model.py
+++ unit_tests/test_borrower_model.py
@@ -46,3 +46,30 @@ class TestBorrowerModel(unittest.TestCase):
         res = borrower.update_borrower_by_id(updated_borrower, "AAAA")
 
         self.assertEqual(res.forename, "Frank")
+
+    @mock.patch('application.borrower.model.Borrower._get_borrower_internal')
+    @mock.patch('application.borrower.model.db', autospec=True)
+    def test_update_borrower_by_id_token_mismatch(self, mock_db, mock_query):
+
+        updated_borrower = copy.deepcopy(DeedHelper._valid_single_borrower_update)
+
+        testBorrower = Borrower()
+
+        testBorrower.id = updated_borrower['id']
+        testBorrower.token = "AAAA"
+        testBorrower.deed_token = "AAAA"
+        testBorrower.forename = updated_borrower['forename']
+        testBorrower.middlename = updated_borrower['middle_name']
+        testBorrower.surname = updated_borrower['surname']
+        testBorrower.dob = updated_borrower['dob']
+        testBorrower.gender = updated_borrower['gender']
+        testBorrower.phonenumber = updated_borrower['phone_number']
+        testBorrower.address = updated_borrower['address']
+        testBorrower.esec_user_name = ""
+
+        mock_query.return_value = testBorrower
+
+        borrower = Borrower()
+        res = borrower.update_borrower_by_id(updated_borrower, "BBBB")
+
+        self.assertEqual(res, "Error Token Mismatch")
