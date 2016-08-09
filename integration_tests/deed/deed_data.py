2acbb59d409c18cd6f0e2a9f121d1fae6ae60f07
Skablam <stainlesssteelmatt@gmail.comc>
2016-08-09 10:05:46 +0100

    Fixed data

--- integration_tests/deed/deed_data.py
+++ integration_tests/deed/deed_data.py
@@ -1,13 +1,13 @@
 valid_deed = {
-    "title_number": "GR999999",
+    "title_number": "GR515835",
     "md_ref": "e-MD12344",
     "property_address": "5 The Drive, This Town, This County, PL4 4TH",
     "identity_checked": "Y",
     "borrowers": [
         {
-            "forename": "Barry",
-            "surname": "Jones",
-            "gender": "Male",
+            "forename": "Ann",
+            "surname": "Smith",
+            "gender": "Female",
             "address": "test address with postcode, PL14 3JR",
             "dob": "23/01/1986",
             "phone_number": "07502154062"
