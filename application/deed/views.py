5e0810e1e2d57c0d2d2dfef2285d2f39fce63d86
Victor Akinmade <victor.akinmade@digital.landregistry.gov.uk>
2016-08-17 15:20:16 +0100

    Fixed title validator

--- application/deed/views.py
+++ application/deed/views.py
@@ -213,7 +213,7 @@ def create():
         return error_message, status.HTTP_400_BAD_REQUEST
 
     validate_title_number = validator.validate_title_number(deed_json)
-    if validate_title_number == "Title does not exist":
+    if validate_title_number != "OK":
         return jsonify({"message": validate_title_number}), status.HTTP_400_BAD_REQUEST
 
     validate_borrower_names, msg = validator.validate_borrower_names(deed_json)
