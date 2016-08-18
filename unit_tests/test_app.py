904afc1a64bd37e92705815533af42ccb3d1216b
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2016-08-18 15:26:58 +0100

    Removal of Do Check test - unable to mock

--- unit_tests/test_app.py
+++ unit_tests/test_app.py
@@ -652,18 +652,6 @@ class TestValidators(TestRoutesBase):
 
         self.assertFalse(result)
 
-    def test_title_validator_do_check_invalid_title(self):
-        title_stub = DeedHelper._invalid_title
-        result = TitleAdaptor.do_check(title_stub)
-
-        self.assertEqual(result, "Title does not exist")
-
-    def test_title_validator_do_check_valid_title(self):
-        title_stub = DeedHelper._invalid_title
-        result = TitleAdaptor.do_check(title_stub)
-
-        self.assertEqual(result, "title OK")
-
 
 class TestCreateDeed(TestRoutesBase):
 
