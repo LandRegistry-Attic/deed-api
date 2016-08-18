e33afcdb0be6b691098b2252ddd3c153cb88211f
Dan Marker <daniel.marker@digital.landregistry.gov.uk>
2016-08-18 14:59:38 +0100

    Add unit tests fix - WIP

--- unit_tests/test_app.py
+++ unit_tests/test_app.py
@@ -653,13 +653,13 @@ class TestValidators(TestRoutesBase):
         self.assertFalse(result)
 
     def test_title_validator_do_check_invalid_title(self):
-        title_stub = DeedHelper._invalid_title["title_number"]
+        title_stub = DeedHelper._invalid_title
         result = TitleAdaptor.do_check(title_stub)
 
         self.assertEqual(result, "Title does not exist")
 
     def test_title_validator_do_check_valid_title(self):
-        title_stub = DeedHelper._json_doc["title_number"]
+        title_stub = DeedHelper._invalid_title
         result = TitleAdaptor.do_check(title_stub)
 
         self.assertEqual(result, "title OK")
