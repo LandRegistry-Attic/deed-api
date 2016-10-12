3036530893d1ac5d1cdf7ffe6c4d3d880c368ec0
Ramin <ramin.vazir@digital.landregistry.gov.uk>
2016-10-12 09:50:29 +0100

    added db table and endpoint to log accepting naa

---
+++ application/naa_audit/model.py
@@ -0,0 +1,15 @@
+from application import db
+
+
+
+class NAAAudit(db.Model):
+    __tablename__ = 'naa_audit'
+
+    id = db.Column(db.Integer, primary_key=True)
+    borrower_id = db.Column(db.Integer, nullable=False)
+    date_accepted = db.Column(db.DateTime, nullable=False)
+
+
+    def save(self):  # pragma: no cover
+        db.session.add(self)
+        db.session.commit()
---
+++ application/naa_audit/views.py
@@ -0,0 +1,24 @@
+from application.naa_audit.model import NAAAudit
+from flask import Blueprint, request
+from flask.ext.api import status
+import datetime
+
+
+naa_bp = Blueprint('naa', __name__,
+                        template_folder='templates',
+                        static_folder='static')
+
+
+@naa_bp.route('/hello', methods=['GET'])
+def hello():
+    return "oh hey there friend"
+
+@naa_bp.route('/accept/<borrower_id>', methods=['POST'])
+def accept_naa(borrower_id):
+    naa = NAAAudit()
+    naa.borrower_id = int(borrower_id)
+    naa.date_accepted = datetime.datetime.now()
+
+    naa.save()
+
+    return '', status.HTTP_200_OK
