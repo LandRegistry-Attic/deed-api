8be0ef7454d7bcc4528e161ee24b4136eacba0b2
Ramin <ramin.vazir@digital.landregistry.gov.uk>
2016-10-17 09:27:13 +0100

    flake8 changes

--- application/naa_audit/views.py
+++ application/naa_audit/views.py
@@ -1,12 +1,11 @@
 from application.naa_audit.model import NAAAudit
-from flask import Blueprint, request
+from flask import Blueprint
 from flask.ext.api import status
 import datetime
 
 
-naa_bp = Blueprint('naa', __name__,
-                        template_folder='templates',
-                        static_folder='static')
+naa_bp = Blueprint('naa', __name__, template_folder='templates', static_folder='static')
+
 
 @naa_bp.route('/accept/<borrower_id>', methods=['POST'])
 def accept_naa(borrower_id):
--- migrations/versions/48bd8328605_.py
+++ migrations/versions/48bd8328605_.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 """empty message
 
 Revision ID: 48bd8328605
