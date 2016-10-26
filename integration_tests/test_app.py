ccbc4c5990223f197b26701215a6b43ab807329f
Adam <adam@LRAdmins-MacBook-Pro.local>
2016-10-26 11:59:46 +0100

    minor changes to int test

--- integration_tests/test_app.py
+++ integration_tests/test_app.py
@@ -12,7 +12,7 @@ class TestAppRoutes(unittest.TestCase):
         setUpApp(self)
 
     def test_health_service_check(self):
-        test_health_check = requests.get(config.DEED_API_BASE_HOST + '/health/service-check')
+        test_health_check = requests.get(config.DEED_API_BASE_HOST + "/health/service-check")
         test_health_list = json.loads(test_health_check.text)
 
         self.assertEqual(test_health_check.status_code, 200)
@@ -21,25 +21,22 @@ class TestAppRoutes(unittest.TestCase):
 
         # Assert that there is a minimum of 4 service responses
         # Which are: register/title adapter, esec-client and postgres
-        if(len(test_health_list["services"]) >= 4):
-            expectedAmount = True
-
-        self.assertEqual(expectedAmount, True)
+        self.assertEqual(True, True) if len(test_health_list["services"]) >= 4 else self.assertEqual(False, True)
 
         # Assert that there is only one "services" tag; which is essentially a tag
         # that has an array of services as a value
         self.assertEqual(len(test_health_list), 1)
 
         # Test that certain tags/values are present
-        self.assertIn("deed-api", str(test_health_list['services']))
-        self.assertIn("title-adapter", str(test_health_list['services']))
-        self.assertIn("register-adapter", str(test_health_list['services']))
-        self.assertIn("esec-client", str(test_health_list['services']))
+        self.assertIn("deed-api", str(test_health_list["services"]))
+        self.assertIn("title-adapter", str(test_health_list["services"]))
+        self.assertIn("register-adapter", str(test_health_list["services"]))
+        self.assertIn("esec-client", str(test_health_list["services"]))
 
-        self.assertIn("service_from", str(test_health_list['services']))
-        self.assertIn("service_to", str(test_health_list['services']))
-        self.assertIn("status_code", str(test_health_list['services']))
-        self.assertIn("service_message", str(test_health_list['services']))
+        self.assertIn("service_from", str(test_health_list["services"]))
+        self.assertIn("service_to", str(test_health_list["services"]))
+        self.assertIn("status_code", str(test_health_list["services"]))
+        self.assertIn("service_message", str(test_health_list["services"]))
 
         # Test that the return json is not empty
         self.assertIsNotNone(test_health_list)
