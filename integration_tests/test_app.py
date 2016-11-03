b972cd032a055f558a06daf4dd32bd66bc050d9f
rachael.tordoff <rachael.tordoff@digital.landregistry.gov.uk>
2016-11-03 11:21:39 +0000

    changes to integration tests

--- integration_tests/test_app.py
+++ integration_tests/test_app.py
@@ -4,9 +4,14 @@ import json
 
 from application import config
 from integration_tests.helper import setUpApp
+from integration_tests.deed.deed_data import valid_deed
 
 
 class TestAppRoutes(unittest.TestCase):
+    webseal_headers = {
+        "Content-Type": "application/json",
+        "Iv-User-L": "CN=DigitalMortgage%20DigitalMortgage,OU=devices,O=testusers,O=1359.2.1,C=gb"
+    }
 
     def setUp(self):
         setUpApp(self)
@@ -36,3 +41,15 @@ class TestAppRoutes(unittest.TestCase):
 
         # Test that the returned json is converted to a dictionary correctly
         self.assertIsInstance(test_health_list, dict)
+
+    def test_get_deed_by_status(self):
+        create_deed = requests.post(config.DEED_API_BASE_HOST + '/deed/',
+                                    data=json.dumps(valid_deed),
+                                    headers=self.webseal_headers)
+        self.assertEqual(create_deed.status_code, 201)
+
+        get_deeds = requests.get(config.DEED_API_BASE_HOST + '/dashboard/DRAFT',
+                                 headers=self.webseal_headers)
+        self.assertEqual(get_deeds.status_code, 200)
+
+        self.assertGreater(int(get_deeds.text), 0)
