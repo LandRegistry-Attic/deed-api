b8f3a8b93bf4c0cdb60e81b6568e7829642004c8
Adam <adam@LRAdmins-MacBook-Pro.local>
2016-12-07 09:39:05 +0000

    changed integration test for organisation name

--- application/service_clients/organisation_adapter/implementation.py
+++ application/service_clients/organisation_adapter/implementation.py
@@ -12,11 +12,11 @@ def get_organisation_name(organisation_id):
     # Connect to organisation-api and get the name for the id provided
     LOGGER.info("Calling get-organisation-name...")
     request_url = reduce(urljoin, [config.ORGANISATION_API_BASE_HOST,
-                                   'get-organisation-name/', organisation_id])
+                                   'organisation-name/', organisation_id])
 
     # Collect the organisation name and return it
     json_response = json.loads(requests.get(request_url).text)
-    organisation_name = json_response["app"]  # change to organisation_name
+    organisation_name = json_response["organisation_name"]
     LOGGER.debug("Response: '%s'", organisation_name)
 
     return organisation_name
