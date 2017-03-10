cf3b78a126cb515ab46839ba2ec57e2aa30d7e10
Adam <adam@LRAdmins-MacBook-Pro.local>
2017-03-10 14:20:17 +0000

    updated int tests

--- integration_tests/deed/deed_data.py
+++ integration_tests/deed/deed_data.py
@@ -33,6 +33,42 @@ valid_deed_with_reference = {
     "reference": "test"
 }
 
+valid_deed_with_date_of_mortgage_offer = {
+    "title_number": "CYM123457",
+    "md_ref": "e-MD1291A",
+    "property_address": "5 The Drive, This Town, This County, PL4 4TH",
+    "borrowers": [{
+        "forename": "Ann",
+        "surname": "Smith",
+        "gender": "Male",
+        "address": "test address with postcode, PL14 3JR",
+        "dob": "23/01/1987",
+        "phone_number": "07502154069"
+    }],
+    "identity_checked": "Y",
+    "reference": "123",
+    "date_of_mortgage_offer": "a date string",
+    "miscellaneous_information": "A Conveyancer"
+}
+
+valid_deed_with_miscellaneous_info = {
+    "title_number": "CYM123457",
+    "md_ref": "e-MD1291A",
+    "property_address": "5 The Drive, This Town, This County, PL4 4TH",
+    "borrowers": [{
+        "forename": "Ann",
+        "surname": "Smith",
+        "gender": "Male",
+        "address": "test address with postcode, PL14 3JR",
+        "dob": "23/01/1987",
+        "phone_number": "07502154069"
+    }],
+    "identity_checked": "Y",
+    "reference": "123",
+    "date_of_mortgage_offer": "a date string",
+    "miscellaneous_information": "A Conveyancer"
+}
+
 new_deed = {
     "title_number": "CYM123457",
     "md_ref": "e-MD12344",
