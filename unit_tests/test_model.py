f5e86979215973807d88233b40b33cb1b0cd20f8
Stuart McColl <stuart.mccoll@landregistry.gov.uk>
2017-06-14 16:01:02 +0100

    Addition of two new unit tests to test use_system routes within the deed model

--- unit_tests/test_model.py
+++ unit_tests/test_model.py
@@ -23,6 +23,17 @@ class TestDeed(unittest.TestCase):
         self.assertEqual(deed.token, result.token)
         self.assertEqual(deed.status, result.status)
 
+    @mock.patch('application.deed.model.Deed.get_deed_system')
+    def test_get_deed_adapter_use_system(self, mock_get_deed_system):
+        deed = Deed()
+        deed.deed = {}
+        deed.token = 'token'
+        deed.status = 'status'
+        mock_get_deed_system.return_value = deed
+        result = deed_adapter('1234', True)
+        self.assertEqual(deed.token, result.token)
+        self.assertEqual(deed.status, result.status)
+
     @mock.patch('application.deed.model.Deed.get_deed')
     def test_get_deed_json_adapter(self, mock_get_deed):
         deed = Deed()
@@ -43,6 +54,20 @@ class TestDeed(unittest.TestCase):
         deed.status = 'status'
         mock_get_deed.return_value = deed
         result = deed_pdf_adapter('1234')
+        self.assertEqual(deed.token, result['token'])
+        self.assertEqual(deed.status, result['status'])
+        self.assertEqual('05/07/2016', result['effective_date'])
+        self.assertEqual(['0 The Drive', 'This Town', 'This County', 'PL0 0TH'], result['property_address'])
+
+    @mock.patch('application.deed.model.Deed.get_deed_system')
+    def test_deed_pdf_adapter_use_system(self, mock_get_deed_system):
+        deed = Deed()
+        deed.deed = {"effective_date": "2016-07-05 12:00:05",
+                     "property_address": "0 The Drive, This Town, This County, PL0 0TH"}
+        deed.token = 'token'
+        deed.status = 'status'
+        mock_get_deed_system.return_value = deed
+        result = deed_pdf_adapter('1234', True)
         self.assertEqual(deed.token, result['token'])
         self.assertEqual(deed.status, result['status'])
         self.assertEqual('05/07/2016', result['effective_date'])
