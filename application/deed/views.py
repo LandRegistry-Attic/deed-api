14150ff19ff1bcd14b717b4c94394826177074ba
treacle74 <ptrelease@gmail.com>
2017-07-10 13:02:57 +0100

    added conveyancer validation and used outcome to submit the correct organisation name to Akuma

--- application/deed/views.py
+++ application/deed/views.py
@@ -2,22 +2,22 @@ import collections
 import copy
 import json
 import sys
-import application
-from datetime import datetime
-
-from application import esec_client
 from application.akuma.service import Akuma
-from application.borrower.model import Borrower
 from application.deed.deed_render import create_deed_pdf
+from application.deed.deed_validator import Validation
 from application.deed.model import Deed, deed_json_adapter, deed_pdf_adapter, deed_adapter
 from application.deed.service import update_deed, update_deed_signature_timestamp, apply_registrar_signature, \
     make_deed_effective_date
-from application.service_clients.organisation_adapter import make_organisation_adapter_client
 from application.deed.utils import convert_json_to_xml
-from application.deed.deed_validator import Validation
+from datetime import datetime
 from flask import Blueprint
 from flask import request, abort, jsonify, Response
 from flask.ext.api import status
+
+import application
+from application import esec_client
+from application.borrower.model import Borrower
+from application.service_clients.organisation_adapter import make_organisation_adapter_client
 
 deed_bp = Blueprint('deed', __name__,
                     template_folder='templates',
@@ -368,6 +368,10 @@ def retrieve_signed_deed():
 
 @deed_bp.route('/<deed_reference>/make-effective', methods=['POST'])
 def make_effective(deed_reference):
+    credentials = Validation().validate_organisation_credentials()
+    if credentials is None:
+        return '', status.HTTP_401_UNAUTHORIZED
+
     deed = Deed()
     result = deed.get_deed(deed_reference)
     if result is None:
@@ -378,7 +382,8 @@ def make_effective(deed_reference):
         deed_status = str(result.status)
 
         if deed_status == "ALL-SIGNED":
-            check_result = Akuma.do_check(result.deed, "make effective", result.organisation_id, result.organisation_name, result.token)
+            check_result = Akuma.do_check(result.deed, "make effective", credentials['organisation_name'],
+                                          credentials['organisation_locale'], result.token)
             application.app.logger.info("Check ID - Make Effective: " + check_result['id'])
 
             signed_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
