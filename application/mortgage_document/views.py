cfd8150f454915cd3bb8527616a3b97c8103f89e
Ramin <ramin.vazir@digital.landregistry.gov.uk>
2017-07-13 14:55:23 +0100

    added use case for not found

--- application/mortgage_document/views.py
+++ application/mortgage_document/views.py
@@ -13,6 +13,10 @@ mortgage_document_bp = Blueprint('mortgage_document', __name__,
 def get_legal_warning(md_ref):
 
     legal_warning = MortgageDocument.get_by_md(md_ref)
-    return legal_warning.legal_warning
+    if legal_warning:
+        if legal_warning.legal_warning:
+            return legal_warning.legal_warning, status.HTTP_200_OK
 
+
+    return "", status.HTTP_404_NOT_FOUND
 
