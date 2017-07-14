bf67c253ca357c603426dcde525363c9bb9c4c12
Stuart McColl <stuart.mccoll@landregistry.gov.uk>
2017-07-14 16:12:33 +0100

    Added unit test for the second if condition within the auth_sms function

--- unit_tests/test_auth_sms.py
+++ unit_tests/test_auth_sms.py
@@ -4,11 +4,12 @@ from unittest.mock import patch, MagicMock
 from application.deed.views import auth_sms
 from application import app
 from flask.ext.api import status
+from unit_tests.helper import DeedModelMock
 
 
 class TestAuthSms(unittest.TestCase):
 
-    @mock.patch('application.deed.model.Deed.get_deed')
+    @mock.patch('application.deed.views.Deed.get_deed')
     @mock.patch('application.deed.views.abort')
     def test_auth_sms_deed_not_found(self, mock_abort, mock_get_deed):
         with app.app_context() as ac:
@@ -17,3 +18,17 @@ class TestAuthSms(unittest.TestCase):
                 mock_get_deed.return_value = None
                 auth_sms("11", "11", "11")
                 mock_abort.assert_called_with(status.HTTP_404_NOT_FOUND)
+
+    @mock.patch('application.deed.views.Akuma.do_check')
+    @mock.patch('application.deed.views.Deed.get_deed')
+    def test_auth_sms_akuma_result_z(self, mock_get_deed, mock_akuma):
+        with app.app_context() as ac:
+            ac.g.trace_id = None
+            with app.test_request_context():
+                mock_get_deed.return_value = DeedModelMock()
+                mock_akuma.return_value = {
+                    "result": "Z",
+                    "id": "2b9115b2-d956-11e5-942f-08002719cd16"
+                }
+                result = auth_sms("11", "11", "11")
+                self.assertEqual(result, 'Failed to sign Mortgage document')
